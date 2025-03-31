92cc7da7fe5238887f3cf1540eac7eedec9f8d90
glory-yun <yyk1797@gmail.com>
2025-04-01 00:36:01 +0900

    csrf 실습

---
+++ csrf_demo/urls.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.urls import path
+from main import views
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('login/', views.user_login),
+    path('write/', views.write_post),
+]
+
+
+urlpatterns += [
+    path('attack/', views.csrf_attack),
+]
---
+++ main/admin.py
@@ -0,0 +1,9 @@
+# Register your models here.
+from django.contrib import admin
+from .models import Post
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'author', 'created_at')
+    search_fields = ('title', 'content', 'author__username')
